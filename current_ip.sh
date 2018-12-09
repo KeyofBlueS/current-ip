c4d79e75967b0b7c8d95064318abbb5325a2d53d
KeyofBlueS <45725677+KeyofBlueS@users.noreply.github.com>
2018-12-09 08:58:37 +0100

    Update current_ip.sh

--- current_ip.sh
+++ current_ip.sh
@@ -176,6 +176,46 @@ Indirizzo ip ($NUM) non reperibile, il sito è\e[0m" "\e[1;31mOFFLINE o rete non
 			fi
 	}
 
+help(){
+echo "
+Questo script permette ad un server di reperire il proprio indirizzo ip pubblico (attualmente, per rindondanza, con quattro
+metodi distinti), salvarlo in locale su un file ed eventualmente inviare quest'ultimo all'esterno. Utile principalmente se
+il fornitore della connessione internet del server assegna un indirizzo ip dinamico, di conseguenza un client deve
+conoscere l'attuale indirizzo ip pubblico del server per poter effettuare una connessione (ad esempio ssh).
+
+Nella SEZIONE CONFIGURAZIONE dello script è possibile impostare il percorso locale in cui verrà salvato il file (contenente
+gli indirizzi ip del server) che verrà generato da questo script (di default è $HOME/).
+Ma più importante è inserire un metodo valido per l'upload del file contenente gli indirizzi ip del server. current-ip non
+possiede alcun metodo di default, lascio all'utente l'inserimento del proprio metodo più congeniale per l'invio del file
+(ad esempio tramite email, upload su un server ftp, upload su un servizio cloud ecc...)
+
+È possibile utilizzare le seguenti opzioni:
+--menu        Avvia il menu principale.
+
+--current-1   Reperisce l'indirizzo ip con il 1° metodo, se l'indirizzo è cambiato aggiorna il file contenente il
+              relativo indrizzo ed avvia la procedura di invio del suddetto file tramite il metodo impostato nella
+              SEZIONE CONFIGURAZIONE.
+
+--current-2   Reperisce l'indirizzo ip con il 2° metodo, se l'indirizzo è cambiato aggiorna il file contenente il
+              relativo indrizzo ed avvia la procedura di invio del suddetto file tramite il metodo impostato nella
+              SEZIONE CONFIGURAZIONE.
+
+--current-3   Reperisce l'indirizzo ip con il 3° metodo, se l'indirizzo è cambiato aggiorna il file contenente il
+              relativo indrizzo ed avvia la procedura di invio del suddetto file tramite il metodo impostato nella
+              SEZIONE CONFIGURAZIONE.
+
+--current-4   Reperisce l'indirizzo ip con il 4° metodo, se l'indirizzo è cambiato aggiorna il file contenente il
+              relativo indrizzo ed avvia la procedura di invio del suddetto file tramite il metodo impostato nella
+              SEZIONE CONFIGURAZIONE.
+
+--send-ip     Avvia la procedura di invio del file contenente gli indirizzi ip del server, tramite il metodo impostato
+              nella SEZIONE CONFIGURAZIONE.
+
+--help        Visualizza una descrizione ed opzioni di current-ip
+"
+exit 0
+	}
+
 if [ "$1" = "--menu" ]
 then
    menu
@@ -194,8 +234,15 @@ then
 elif [ "$1" = "--send-ip" ]
 then
    send_ip
+elif [ "$1" = "--help" ]
+then
+   help
 else
    menu
+#   current_ip_1
 #   current_ip_2
-#   current_ip_2
+#   current_ip_3
+#   current_ip_4
+#   send_ip
+#   help
 fi
