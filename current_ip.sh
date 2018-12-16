00b5f51bec9f96f83fdb05fd29ee362940b7058b
KeyofBlueS <45725677+KeyofBlueS@users.noreply.github.com>
2018-12-16 12:13:46 +0100

    Update current_ip.sh

--- current_ip.sh
+++ current_ip.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Version:    1.0.0
+# Version:    1.5.0
 # Author:     KeyofBlueS
 # Repository: https://github.com/KeyofBlueS/current-ip
 # License:    GNU General Public License v3.0, https://opensource.org/licenses/GPL-3.0
@@ -8,6 +8,7 @@
 ######################################## INIZIO SEZIONE CONFIGURAZIONE #####################################################################
 
 ### Inserire il percorso locale in cui verrà salvato il file (contenente gli indirizzi ip del server) che verrà generato da questo script
+# (default "$HOME/")
 CURRENT_PATH="$HOME/"
 
 ### Nella seguente funzione, inserire il metodo più congeniale per l'invio del file contenente gli indirizzi ip del server
@@ -20,7 +21,7 @@ exit 0
 
 ######################################### FINE SEZIONE CONFIGURAZIONE ######################################################################
 
-for name in curl
+for name in curl dig hostname sed wget
 do
   [[ $(which $name 2>/dev/null) ]] || { echo -en "\n$name è richiesto da questo script. Utilizza 'sudo apt-get install $name'";deps=1; }
 done
@@ -28,26 +29,97 @@ done
 
 CURRENT_FILE="$LOGNAME"@"$HOSTNAME"_current.txt
 CURRENT="$CURRENT_PATH$CURRENT_FILE"
-echo -e "\e[1;34m## Indirizzi IP correnti: ##\e[0m"
-cat $CURRENT | grep "export SERVERIP_INTERNET_"
+cat "$CURRENT" | grep -q "export "
 if [ $? = 0 ]; then
 echo
 else
 echo "$CURRENT_FILE non presente nel percorso specificato, procedo a creare la configurazione iniziale"
 mkdir -p $CURRENT_PATH
-echo "export SERVERIP_INTERNET_1=0.0.0.0
+echo "export SSHPORT=22
+export SERVERUSERNAME=$LOGNAME
+export SERVERHOSTNAME=$HOSTNAME
+export SERVERIP_LAN_1=0.0.0.0
+export SERVERIP_INTERNET_1=0.0.0.0
 export SERVERIP_INTERNET_2=0.0.0.0
 export SERVERIP_INTERNET_3=0.0.0.0
-export SERVERIP_INTERNET_4=0.0.0.0" > "$CURRENT"
+export SERVERIP_INTERNET_4=0.0.0.0
+" > "$CURRENT"
 chmod +x "$CURRENT"
 fi
 
+# Controllo porta ssh in ascolto
+CURRENTSSHPORT=`cat "/etc/ssh/sshd_config" | grep '^Port ' | grep -E -o '([0-9]{1,4}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])'`
+SSHPORT=`cat "$CURRENT" | grep "export SSHPORT="`
+echo $CURRENTSSHPORT | grep -E -o -q '([0-9]{1,4}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])'
+if [ $? = 0 ]; then
+	echo $SSHPORT | grep -q "export SSHPORT=$CURRENTSSHPORT"
+	if [ $? = 0 ]; then
+		echo -e "\e[1;34mLa porta ssh in ascolto non è cambiata.\e[0m"
+	else
+		echo -e "\e[1;31mPorta ssh in ascolto aggiornata:" "\e[1;34m "$CURRENTSSHPORT"\e[0m"
+		sed s/"$SSHPORT"/"export SSHPORT=$CURRENTSSHPORT"/ < "$CURRENT" > $HOME/.currentsshport
+		mv $HOME/.currentsshport "$CURRENT"
+	fi
+else
+	echo -e "\e[1;31m	Porta ssh in ascolto non reperibile\e[0m"
+	echo $SSHPORT | grep -q "export SSHPORT=22"
+	if [ $? = 0 ]; then
+		echo -e "\e[1;34mLa porta ssh in ascolto non è cambiata.\e[0m"
+	else
+		echo -e "\e[1;31mPorta ssh in ascolto aggiornata:" "\e[1;34m "22 "(default)""\e[0m"
+		sed s/"$SSHPORT"/"export SSHPORT=22"/ < "$CURRENT" > $HOME/.currentsshport
+		mv $HOME/.currentsshport "$CURRENT"
+	fi
+fi
+
+# Controllo nome utente del server
+SERVERUSERNAME=`cat "$CURRENT" | grep "export SERVERUSERNAME="`
+cat "$CURRENT" | grep "export SERVERUSERNAME=" | grep -q "$LOGNAME"
+if [ $? = 0 ]; then
+	echo -e "\e[1;34mIl nome utente del server non è cambiato.\e[0m"
+else
+	echo -e "\e[1;31mNome utente del server aggiornato:" "\e[1;34m "$LOGNAME"\e[0m"
+	sed s/"$SERVERUSERNAME"/"export SERVERUSERNAME=$LOGNAME"/ < "$CURRENT" > $HOME/.currentuser
+	mv $HOME/.currentuser "$CURRENT"
+fi
+
+# Controllo nome host del server
+SERVERHOSTNAME=`cat "$CURRENT" | grep "export SERVERHOSTNAME="`
+cat "$CURRENT" | grep "export SERVERHOSTNAME=" | grep -q "$HOSTNAME"
+if [ $? = 0 ]; then
+	echo -e "\e[1;34mIl nome host del server non è cambiato.\e[0m"
+else
+	echo -e "\e[1;31mNome host del server aggiornato" "\e[1;34m "$HOSTNAME"\e[0m"
+	sed s/"$SERVERHOSTNAME"/"export SERVERHOSTNAME=$HOSTNAME"/ < "$CURRENT" > $HOME/.currenthost
+	mv $HOME/.currenthost "$CURRENT"
+fi
+
+# Controllo indirizzo/i ip lan del server
+CURRENTSERVERIP_LAN_1=`hostname -I | grep -E -o '(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)'`
+SERVERIP_LAN_1=`cat "$CURRENT" | grep "export SERVERIP_LAN_1="`
+echo $CURRENTSERVERIP_LAN_1 | grep -E -o -q '(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)'
+if [ $? = 0 ]; then
+	echo $SERVERIP_LAN_1 | grep -q "export SERVERIP_LAN_1=$CURRENTSERVERIP_LAN_1"
+	if [ $? = 0 ]; then
+		echo -e "\e[1;34mL'indirizzo ip locale non è cambiato.\e[0m"
+	else
+		echo -e "\e[1;31mIndirizzo ip locale aggiornato:" "\e[1;34m "$CURRENTSERVERIP_LAN_1"\e[0m"
+		sed s/"$SERVERIP_LAN_1"/"export SERVERIP_LAN_1=$CURRENTSERVERIP_LAN_1"/ < "$CURRENT" > $HOME/.currentlocalip
+		mv $HOME/.currentlocalip "$CURRENT"
+	fi
+else
+	echo -e "\e[1;31m	L'indirizzo ip locale non è reperibile, lascio il precedente\e[0m"
+fi
+echo -e "\e[1;34m## Indirizzi IP correnti: ##\e[0m"
+cat "$CURRENT"
+
+# Configurazioni per il reperimento dell'indirizzo ip pubblico
 config_1(){
 NUM=1
 echo -e "\e[1;34m## Hai scelto ifconfig.me - current ip 1 ##
 ## Provo a reperire il tuo indirizzo ip da ifconfig.me... ##\e[0m"
 declare -a cmdArgs='([0]="curl" [1]="http://ifconfig.me/ip")'
-current_ip
+check_ip
 }
 
 config_2(){
@@ -55,7 +127,7 @@ NUM=2
 echo -e "\e[1;34m## Hai scelto ipecho.net - current ip 2 ##
 ##  Provo a reperire il tuo indirizzo ip da ipecho.net... ##\e[0m"
 declare -a cmdArgs='([0]="wget" [1]="-qO-" [2]="http://ipecho.net/plain")'
-current_ip
+check_ip
 }
 
 config_3(){
@@ -63,7 +135,7 @@ NUM=3
 echo -e "\e[1;34m## Hai scelto opendns.com - current ip 3 ##
 ##  Provo a reperire il tuo indirizzo ip da opendns.com... ##\e[0m"
 declare -a cmdArgs='([0]="dig" [1]="+short" [2]="myip.opendns.com" [3]="@resolver1.opendns.com")'
-current_ip
+check_ip
 }
 
 config_4(){
@@ -71,22 +143,6 @@ NUM=4
 echo -e "\e[1;34m## Hai scelto google.com - current ip 4 ##
 ##  Provo a reperire il tuo indirizzo ip da google.com... ##\e[0m"
 declare -a cmdArgs='([0]="dig" [1]="TXT" [2]="+short" [3]="o-o.myaddr.l.google.com" [4]="@ns1.google.com")'
-current_ip
-}
-
-current_ip(){
-for pid in $(pgrep -f "current-ip --current-$NUM"); do
-    if [ $pid != $$ ]; then
-        kill -9 $pid
-    fi 
-done
-CURRENTTMP=$HOME/.current"$NUM"_tmp
-echo "" > "$CURRENTTMP"
-CURRENTCHECK=`cat "$CURRENT" | grep "export SERVERIP_INTERNET_"$NUM`
-"${cmdArgs[@]}" > "$CURRENTTMP"
-CURRENTTMPIP=`cat "$CURRENTTMP" | grep -E -o '(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)'`
-echo "SERVERIP_INTERNET_1=$CURRENTTMPIP" > "$CURRENTTMP"
-CURRENT_IP=`cat "$CURRENT" | grep "export SERVERIP_INTERNET_$NUM="`
 check_ip
 }
 
@@ -132,56 +188,57 @@ case $testo in
     ;;
     E|e)
 	{
-			echo -e "\e[1;34mEsco dal programma\e[0m"
-			exit 0
+	echo -e "\e[1;34mEsco dal programma\e[0m"
+	exit 0
 	}
     ;;
     *)
-echo -e "\e[1;31mHai sbagliato tasto.......cerca di stare un po' attento\e[0m"
+	echo -e "\e[1;31mHai sbagliato tasto.......cerca di stare un po' attento\e[0m"
     menu
     ;;
 esac
-	}
+}
 
+# Controllo indirizzo ip pubblico del server
 check_ip(){
-			while true
-			do
-				echo -e "\e[1;34m## Il tuo indirizzo ip ($NUM) è: ##\e[0m" && cat "$CURRENTTMP" | grep -E -o '(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)'
-				if [ $? = 0 ]; then
-					break
-				fi
-					echo -e "\e[1;34m
-Indirizzo ip ($NUM) non reperibile, il sito è\e[0m" "\e[1;31mOFFLINE o rete non raggiungibile\e[0m"
-					echo -e "\e[1;31mPremi INVIO per uscire, o attendi 10 minuti per riprovare automaticamente\e[0m"
-				if read -t 600 _e; then
-					exit 0
-				fi
-					config_$NUM
-					done
-			cat "$CURRENT" | grep -xqFe "export SERVERIP_INTERNET_$NUM=$CURRENTTMPIP"
-			  if [ $? = 0 ]
-			  then
-				rm "$CURRENTTMP"
+for pid in $(pgrep -f "current-ip --current-$NUM"); do
+    if [ $pid != $$ ]; then
+        kill -9 $pid
+    fi 
+done
+CURRENTTMPIP=`"${cmdArgs[@]}" | grep -E -o '(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)'`
+CURRENT_IP=`cat "$CURRENT" | grep "export SERVERIP_INTERNET_$NUM="`
+echo -e "\e[1;34m## Il tuo indirizzo ip ($NUM) è: ##\e[0m" && echo "$CURRENTTMPIP" | grep -E -o '(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)'
+if [ $? = 0 ]; then
+		cat "$CURRENT" | grep -xqFe "export SERVERIP_INTERNET_$NUM=$CURRENTTMPIP"
+		if [ $? = 0 ]; then
 				echo -e "\e[1;34m## Indirizzi IP correnti: ##\e[0m"
 				cat "$CURRENT"
-				echo -e "\e[1;31mL'indirizzo ip ($NUM) non è cambiato, esco...\e[0m"
+				echo -e "\e[1;34mL'indirizzo ip ($NUM) non è cambiato, esco...\e[0m"
 				exit 0
-			  else
-				echo -e "\e[1;31mIndirizzo ip ($NUM) aggiornato\e[0m"
+		else
+				echo -e "\e[1;31mIndirizzo ip ($NUM) aggiornato:" "\e[1;34m "$CURRENTTMPIP"\e[0m"
 				sed s/"$CURRENT_IP"/"export SERVERIP_INTERNET_$NUM=$CURRENTTMPIP"/ < "$CURRENT" > $HOME/.current
 				mv $HOME/.current "$CURRENT"
-				#mv -f $CURRENTTMP $CURRENT
-				#rm $CURRENTTMP
 				cat "$CURRENT"
 				send_ip
-			fi
-	}
+		fi
+else
+		echo -e "\e[1;31m
+Indirizzo ip ($NUM) non reperibile, il sito è\e[0m" "\e[1;31mOFFLINE o rete non raggiungibile\e[0m"
+		echo -e "\e[1;31mPremi INVIO per uscire, o attendi 10 minuti per riprovare automaticamente\e[0m"
+		if read -t 600 _e; then
+				exit 0
+		fi
+config_$NUM
+fi
+}
 
 givemehelp(){
 echo "
 # current-ip
 
-# Version:    1.0.0
+# Version:    1.5.0
 # Author:     KeyofBlueS
 # Repository: https://github.com/KeyofBlueS/current-ip
 # License:    GNU General Public License v3.0, https://opensource.org/licenses/GPL-3.0
@@ -191,6 +248,11 @@ Questo script permette ad un server di reperire il proprio indirizzo ip pubblico
 metodi distinti), salvarlo in locale su un file ed eventualmente inviare quest'ultimo all'esterno. Utile principalmente se
 il fornitore della connessione internet del server assegna un indirizzo ip dinamico, di conseguenza un client deve
 conoscere l'attuale indirizzo ip pubblico del server per poter effettuare una connessione (ad esempio ssh).
+Oltre all'indirizzo ip pubblico reperisce anche altre informazioni utili per il collegamento verso il server:
+Porta in ascolto del server ssh
+Nome utente del server
+Nome host del server
+Indirizzo IP nella rete locale del server
 
 ### CONFIGURAZIONE
 Nella SEZIONE CONFIGURAZIONE dello script è possibile impostare il percorso locale in cui verrà salvato il file (contenente
@@ -200,14 +262,19 @@ possiede alcun metodo di default, lascio all'utente l'inserimento del proprio me
 (ad esempio tramite email, upload su un server ftp, upload su un servizio cloud ecc...)
 
 ### UTILIZZO
-Per rendere il processo automatico consiglio di impostare crontab nel seguente modo, in modo da interrogare ogni servizio a
-distanza di un'ora:
+Per rendere il processo automatico consiglio di impostare crontab come segue, in modo da interrogare ogni servizio a
+distanza di un'ora. Per configurare crontab, digitare su un terminale:
+
+$ crontab -e
+
+Esempio cronjob:
 
 PATH=/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/bin
 0 * * * * current-ip --current-1 > /dev/null 2>&1 &
 15 * * * * current-ip --current-2 > /dev/null 2>&1 &
 30 * * * * current-ip --current-3 > /dev/null 2>&1 &
 45 * * * * current-ip --current-4 > /dev/null 2>&1 &
+
 
 Per utilizzare manualmente lo script basta digitare su un terminale:
 
@@ -240,7 +307,7 @@ e seguire le istruzioni su schermo.
 --help        Visualizza una descrizione ed opzioni di current-ip
 "
 exit 0
-	}
+}
 
 if [ "$1" = "--menu" ]
 then
@@ -265,10 +332,8 @@ then
    givemehelp
 else
    menu
-#   current_ip_1
-#   current_ip_2
-#   current_ip_3
-#   current_ip_4
-#   send_ip
-#   help
+#  config_1
+#  config_2
+#  config_3
+#  config_4
 fi
